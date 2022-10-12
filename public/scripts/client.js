98e4b57dd0926bc903846e1d567f474563dd2069
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-11 19:52:56 -0700

    Style tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,9 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(() => {
+  const $flag = $('i .fa-regular .fa-flag');
 
+  console.log($flag);
+  
+
+
+
+}); 
