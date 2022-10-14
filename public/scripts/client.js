966e6189c661e43fae6b19c00301cb14da41407f
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-14 09:17:39 -0700

    Bug fix - clear form after submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
   
-  // Cross-Site Scripting prevention
+  // Function: cross-site scripting prevention
   const escapeFunction = function(string, element) {
     let newElement = document.createElement(element);
     newElement.appendChild(document.createTextNode(string));
     return newElement.innerHTML;
   };
   
-  // Builds tweet html from object
+  // Function: builds tweet html from object
   const createTweetElement = (tweetObject) => {
     const $tweet = $(
       `<article class="tweet">
@@ -32,7 +32,7 @@ $(document).ready(() => {
     return $tweet;
   };
 
-  // Loops through array of tweets, builds and renders each onto page
+  // Function: loops through array of tweets, builds and renders each onto page
   const $tweetsContainer =  $('#tweets-container');
   const renderTweets = (tweetsArray) => {
     for (let tweet of tweetsArray) {
@@ -41,24 +41,28 @@ $(document).ready(() => {
     }
   };
 
-  // On page load, AJAX GET to retrieve tweets from database and render
+
+
+
+  // ON PAGE LOAD, AJAX GET to retrieve tweets from database and render
   $.ajax({
     url: '/tweets',
     method: 'GET',
     dataType: 'json',
   })
-    .then((tweets) => {
-      renderTweets(tweets);
-    });
+  .then((tweets) => {
+    renderTweets(tweets);
+  });
   
 
 
     
+  // TWEET COMPOSER HANDLER
   const $newTweetForm = $('#new-tweet-form');
   const $textArea = $('#tweet-text');
   const $errorMessage = $('#error-message');
   
-  // Listen for tweet form submission and prevent default behaviour
+  // Listen for submission and prevent default behaviour
   $newTweetForm.submit(function(event) {
     event.preventDefault();
     
@@ -87,20 +91,24 @@ $(document).ready(() => {
     })
 
     // Refetch tweets from database with AJAX GET
-      .then((data) => {
-        return $.ajax({
-          url: '/tweets',
-          method: 'GET',
-          dataType: 'json',
-        });
-      })
-
-    // Create new tweet element from latest in database and render
-      .then((tweets) => {
-        const $newTweet = createTweetElement(tweets[tweets.length - 1]);
-        $tweetsContainer.prepend($newTweet);
+    .then((data) => {
+      return $.ajax({
+        url: '/tweets',
+        method: 'GET',
+        dataType: 'json',
       });
+    })
+
+    // Create new tweet element and render; clear form
+    .then((tweets) => {
+      const $newTweet = createTweetElement(tweets[tweets.length - 1]);
+      $tweetsContainer.prepend($newTweet);
+      $textArea.val('');
+    });
   });
+
+
+
 
 });
 
